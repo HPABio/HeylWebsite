1c87e71524eeea191ec281842a22c22f4f32f6d5
Hendrik Cooper <biocentra.info@gmail.com>
2025-08-18 18:18:03 +0200

    Refactor import paths "@" to point to /src to use aliasing for improved readability and maintainability; update Astro configuration to support path resolution.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+import path from "path";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
+  vite: {
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "src"),
+      },
+    },
+  },
 });
