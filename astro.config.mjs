c4f45fcdf777eb1c1cbd155ef87c738b5cfdac88
Hendrik Cooper <biocentra.info@gmail.com>
2025-09-04 05:34:58 +0200

    changed <img> tags to <Image>

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,35 +3,34 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import compress from "vite-plugin-compression";
-import viteImagemin from 'vite-plugin-imagemin';
+import viteImagemin from "vite-plugin-imagemin";
 
 export default defineConfig({
   integrations: [react(), tailwind()],
   vite: {
     plugins: [
-
-    viteImagemin({
-      gifsicle: { optimizationLevel: 7 },
-      optipng: { optimizationLevel: 7 },
-      mozjpeg: { quality: 80 },
-      pngquant: { quality: [0.7, 0.9] },
-      svgo: {},
-      webp: { quality: 85 }, // 👈 Converts PNG/JPG → WebP
-    }),//     // Brotli (preferred)
+      viteImagemin({
+        gifsicle: { optimizationLevel: 7 },
+        optipng: { optimizationLevel: 7 },
+        mozjpeg: { quality: 80 },
+        pngquant: { quality: [0.7, 0.9] },
+        svgo: {},
+        webp: { quality: 85 }, // 👈 Converts PNG/JPG → WebP
+      }), //     // Brotli (preferred)
       compress({
         algorithm: "brotliCompress",
         ext: ".br",
         threshold: 10240,
         deleteOriginFile: false,
-        compressionOptions: { level: 11 }
+        compressionOptions: { level: 11 },
       }),
       // Gzip (fallback)
       compress({
         algorithm: "gzip",
         ext: ".gz",
         threshold: 10240,
-        deleteOriginFile: false
-      })
-    ]
-  }
+        deleteOriginFile: false,
+      }),
+    ],
+  },
 });
